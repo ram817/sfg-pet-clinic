7349cf51aab37cbeac40f27386fd9b32b550f2d3
ram817 <>
2019-02-27 15:52:18 +0800

    fix test error path 2

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/model/Owner.java
+++
@@ -1,6 +0,0 @@
-package guru.springframework.sfgpetclinic.model.model;
-
-public class Owner extends Person {
-
-
-}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/model/Vet.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.sfgpetclinic.model.model;
-
-public class Vet extends Person {
-
-
-
-}
